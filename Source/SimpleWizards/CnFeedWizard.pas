b4dabdf3b8a821152a9c5b6674e0588bd9a1f7a4
cnzjy <zjy@cnpack.org>
2010-04-08 09:33:17 +0000

    * [CnFeedWizard] Add feed parser.

--- Source/SimpleWizards/CnFeedWizard.pas
+++ Source/SimpleWizards/CnFeedWizard.pas
@@ -40,7 +40,9 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, IniFiles, Forms,
-  Menus, CommCtrl, ComCtrls, CnWizClasses, CnWizNotifier, CnWizUtils, CnCommon;
+  Menus, CommCtrl, ComCtrls, CnWizClasses, CnWizNotifier, CnWizUtils, CnCommon,
+  CnFeedParser, Contnrs, ExtCtrls, Math, CnWizOptions, CnConsts, CnWizConsts,
+  CnWizMultiLang;
 
 type
 
@@ -80,16 +82,38 @@ type
     property Active: Boolean read FActive write FActive;
   end;
 
+  TCnFeedDefine = record
+    IDStr: string;
+    Url: string;
+    CheckPeriod: Integer;
+    Recent: Integer;
+  end;
+
   TCnFeedWizard = class(TCnIDEEnhanceWizard)
   private
-    FList: TList;
-    procedure EditControlNotify(EditControl: TControl; EditWindow: TCustomForm; 
+    FIni: TCustomIniFile;
+    FFeedPath: string;
+    FFeedDef: array of TCnFeedDefine;
+    FLastUpdateTick: Cardinal;
+    FLastCheckTick: Cardinal;
+    FTimer: TTimer;
+    FPanels: TList;
+    FFeeds: TObjectList;
+    FSortedFeeds: TList;
+    FCurFeed: TCnFeedItem;
+    FUpdatePeriod: Integer;
+    FExludeCategories: string;
+    procedure EditControlNotify(EditControl: TControl; EditWindow: TCustomForm;
       Operation: TOperation);
     procedure DoUpdateStatusPanel(EditWindow: TCustomForm; EditControl: TControl;
       Context: Pointer);
-    function GetCount: Integer;
+    function GetPanelCount: Integer;
     function GetPanels(Index: Integer): TCnStatusPanel;
     procedure PanelClick(Sender: TObject);
+    function GetFeedCount: Integer;
+    function GetFeeds(Index: Integer): TCnFeedChannel;
+    procedure OnTimer(Sender: TObject);
+    function DoUpdateFeed(Def: TCnFeedDefine; ForceUpdate: Boolean): Boolean;
   protected
     procedure SetActive(Value: Boolean); override;
     function GetHasConfig: Boolean; override;
@@ -107,8 +131,17 @@ type
 
     procedure UpdateStatusPanels;
 
-    property Count: Integer read GetCount;
+    procedure NextFeed;
+    function UpdateFeeds(ForceUpdate: Boolean = False): Boolean;
+
+    property PanelCount: Integer read GetPanelCount;
     property Panels[Index: Integer]: TCnStatusPanel read GetPanels;
+    property FeedCount: Integer read GetFeedCount;
+    property Feeds[Index: Integer]: TCnFeedChannel read GetFeeds;
+    property CurFeed: TCnFeedItem read FCurFeed;
+  published
+    property UpdatePeriod: Integer read FUpdatePeriod write FUpdatePeriod default 30;
+    property ExludeCategories: string read FExludeCategories write FExludeCategories;
   end;
 
 implementation
@@ -117,17 +150,34 @@ uses
 {$IFDEF DEBUG}
   CnDebug,
 {$ENDIF}
-  CnEditControlWrapper, CnWizIdeUtils;
+  CnEditControlWrapper, CnWizIdeUtils, CnInetUtils;
 
 const
   SCnFeedStatusPanel = 'CnFeedStatusPanel';
   SCnEdtStatusBar = 'StatusBar';
+  SCnFeedCache = 'FeedCache\';
+  SCnUpdateFeedMutex = 'CnUpdateFeedMutex';
+
+  SCnOfficalFeedIDStr = 'CnPackOfficalFeed';
+  SCnOfficalFeedUrl = 'http://www.cnpack.org/rssbuild.php?lang=en';
+  SCnOfficalFeedPeriod = 24 * 60;
+  SCnOfficalFeedRecent = 10;
 
 {$IFDEF COMPILER6_UP}
   csBarKeepWidth = 140;
 {$ELSE}
   csBarKeepWidth = 80;
 {$ENDIF}
+
+function DoSortFeed(Item1, Item2: Pointer): Integer;
+begin
+  if TCnFeedItem(Item1).PubDate > TCnFeedItem(Item2).PubDate then
+    Result := -1
+  else if TCnFeedItem(Item1).PubDate < TCnFeedItem(Item2).PubDate then
+    Result := 1
+  else
+    Result := 0;
+end;
 
 { TCnStatusPanel }
 
@@ -153,7 +203,6 @@ end;
 constructor TCnStatusPanel.Create(AOwner: TComponent);
 begin
   inherited;
-  Text := 'This is a sample text displayed in status bar. The RSS support is coming soon.';
   InitPopupMenu;
 end;
 
@@ -270,7 +319,19 @@ end;
 constructor TCnFeedWizard.Create;
 begin
   inherited;
-  FList := TList.Create;
+  FLastUpdateTick := GetTickCount;
+  FLastCheckTick := GetTickCount;
+  FPanels := TList.Create;
+  FFeeds := TObjectList.Create;
+  FSortedFeeds := TList.Create;
+  FTimer := TTimer.Create(nil);
+  FTimer.OnTimer := OnTimer;
+  FTimer.Interval := 1000;
+  FTimer.Enabled := True;
+  FUpdatePeriod := 30;
+  FIni := CreateIniFile;
+  FFeedPath := WizOptions.UserPath + SCnFeedCache;
+  ForceDirectories(FFeedPath);
   
   EditControlWrapper.AddEditControlNotifier(EditControlNotify);
   UpdateStatusPanels;
@@ -281,9 +342,13 @@ var
   i: Integer;
 begin
   EditControlWrapper.RemoveEditControlNotifier(EditControlNotify);
-  for i := FList.Count - 1 downto 0 do
-    TCnStatusPanel(FList[i]).Free;
-  FList.Free;
+  for i := FPanels.Count - 1 downto 0 do
+    TCnStatusPanel(FPanels[i]).Free;
+  FPanels.Free;
+  FFeeds.Free;
+  FSortedFeeds.Free;
+  FTimer.Free;
+  FIni.Free;
   inherited;
 end;
 
@@ -310,7 +375,7 @@ begin
           Panel.StatusBar := StatusBar;
           Panel.OnClick := PanelClick;
           Panel.UpdateStatus;
-          FList.Add(Panel);
+          FPanels.Add(Panel);
         {$IFDEF DEBUG}
           CnDebugger.LogMsgWarning('CnStatusPanel installed');
         {$ENDIF}
@@ -342,9 +407,19 @@ begin
   UpdateStatusPanels;
 end;
 
-function TCnFeedWizard.GetCount: Integer;
+function TCnFeedWizard.GetPanelCount: Integer;
 begin
-  Result := FList.Count;
+  Result := FPanels.Count;
+end;
+
+function TCnFeedWizard.GetFeedCount: Integer;
+begin
+  Result := FFeeds.Count;
+end;
+
+function TCnFeedWizard.GetFeeds(Index: Integer): TCnFeedChannel;
+begin
+  Result := TCnFeedChannel(FFeeds[Index]);
 end;
 
 function TCnFeedWizard.GetHasConfig: Boolean;
@@ -354,48 +429,89 @@ end;
 
 function TCnFeedWizard.GetPanels(Index: Integer): TCnStatusPanel;
 begin
-  Result := TCnStatusPanel(FList[Index]);
+  Result := TCnStatusPanel(FPanels[Index]);
 end;
 
 class procedure TCnFeedWizard.GetWizardInfo(var Name, Author, Email,
   Comment: string);
 begin
   Name := 'Feed Wizard';
+  Author := SCnPack_Zjy;
+  Email := SCnPack_ZjyEmail;
+  Comment := 'Feed Wizard';
 end;
 
 procedure TCnFeedWizard.LanguageChanged(Sender: TObject);
+var
+  i: Integer;
 begin
   inherited;
-
+  for i := 0 to PanelCount - 1 do
+    Panels[i].LanguageChanged(Sender);
 end;
 
 procedure TCnFeedWizard.Loaded;
 begin
   inherited;
-
+  UpdateFeeds;
 end;
 
 procedure TCnFeedWizard.LoadSettings(Ini: TCustomIniFile);
+var
+  AName, AIDStr: string;
+  Cnt, i: Integer;
 begin
   inherited;
+  FFeedDef := nil;
+  Cnt := Ini.ReadInteger('', 'DefCount', 0);
+  for i := 0 to Cnt - 1 do
+  begin
+    AName := 'Item' + IntToStr(i);
+    AIDStr := Ini.ReadString(AName, 'IDStr', '');
+    if AIDStr <> '' then
+    begin
+      SetLength(FFeedDef, Length(FFeedDef) + 1);
+      with FFeedDef[High(FFeedDef)] do
+      begin
+        IDStr := AIDStr;
+        Url := Ini.ReadString(AName, 'Url', '');
+        CheckPeriod := Ini.ReadInteger(AName, 'CheckPeriod', 24 * 60);
+        Recent := Ini.ReadInteger(AName, 'Recent', 0);
+      end;
+    end;
+  end;
+end;
 
+procedure TCnFeedWizard.SaveSettings(Ini: TCustomIniFile);
+var
+  AName: string;
+  i: Integer;
+begin
+  inherited;
+  Ini.WriteInteger('', 'DefCount', High(FFeedDef) + 1);
+  for i := 0 to High(FFeedDef) do
+  begin
+    AName := 'Item' + IntToStr(i);
+    with FFeedDef[High(i)] do
+    begin
+      Ini.WriteString(AName, 'IDStr', IDStr);
+      Ini.WriteString(AName, 'Url', Url);
+      Ini.WriteInteger(AName, 'CheckPeriod', CheckPeriod);
+      Ini.WriteInteger(AName, 'Recent', Recent);
+    end;
+  end;
 end;
 
 procedure TCnFeedWizard.PanelClick(Sender: TObject);
 begin
-  InfoDlg('This is a sample text displayed in status bar. The RSS context is coming soon.');
-end;
-
-procedure TCnFeedWizard.SaveSettings(Ini: TCustomIniFile);
-begin
-  inherited;
-
+  if (FCurFeed <> nil) and (FCurFeed.Link <> '') then
+    OpenUrl(FCurFeed.Link);
 end;
 
 procedure TCnFeedWizard.SetActive(Value: Boolean);
 begin
   inherited;
-
+  UpdateStatusPanels;
 end;
 
 procedure TCnFeedWizard.UpdateStatusPanels;
@@ -403,8 +519,152 @@ var
   i: Integer;
 begin
   EnumEditControl(DoUpdateStatusPanel, nil);
-  for i := 0 to Count - 1 do
+  for i := 0 to PanelCount - 1 do
     Panels[i].UpdateStatus;
+end;
+
+procedure TCnFeedWizard.NextFeed;
+var
+  Rnd: Double;
+  i: Integer;
+begin
+  FCurFeed := nil;
+  if FSortedFeeds.Count > 0 then
+  begin
+    Rnd := Power(Random, 2.5);
+    FCurFeed := TCnFeedItem(FSortedFeeds[TrimInt(Round(Rnd * FSortedFeeds.Count),
+      0, FSortedFeeds.Count - 1)]);
+    for i := 0 to PanelCount - 1 do
+      Panels[i].Text := FCurFeed.Title;
+  end;
+  FLastUpdateTick := GetTickCount;
+end;
+
+procedure TCnFeedWizard.OnTimer(Sender: TObject);
+begin
+  if not Active then
+    Exit;
+
+  if Abs(GetTickCount - FLastUpdateTick) >= FUpdatePeriod * 1000 then
+  begin
+    NextFeed;
+  end;
+
+  if Abs(GetTickCount - FLastCheckTick) >= 60 * 1000 then
+  begin
+    if UpdateFeeds then
+      NextFeed;
+  end;
+end;
+
+function TCnFeedWizard.DoUpdateFeed(Def: TCnFeedDefine; ForceUpdate: Boolean): Boolean;
+var
+  FileName, TmpName: string;
+  Feed: TCnFeedChannel;
+  i: Integer;
+  List: TList;
+begin
+  Result := False;
+  
+  Feed := nil;
+  for i := 0 to FeedCount - 1 do
+    if SameText(Feeds[i].IDStr, Def.IDStr) then
+    begin
+      Feed := Feeds[i];
+      Break;
+    end;
+  if Feed = nil then
+  begin
+    Feed := TCnFeedChannel.Create;
+    Feed.IDStr := Def.IDStr;
+    FFeeds.Add(Feed);
+    Result := True;
+  end;
+
+  FileName := FFeedPath + Def.IDStr + '.xml';
+  TmpName := ChangeFileExt(FileName, '.tmp');
+  if ForceUpdate or (Abs(FIni.ReadDateTime('LastCheck', Def.IDStr, 0) - Now) > Def.CheckPeriod / 24 / 60) then
+  begin
+    FIni.WriteDateTime('LastCheck', Def.IDStr, Now);
+    with TCnHTTP.Create do
+    try
+      if GetFile(Def.Url, TmpName) and (GetFileSize(TmpName) > 0) then
+      begin
+        DeleteFile(FileName);
+        CopyFile(PChar(TmpName), PChar(FileName), False);
+        DeleteFile(TmpName);
+      end;
+    finally
+      Free;
+    end;
+    Result := True;
+  end;
+
+  if Result then
+  begin
+    Feed.LoadFromFile(FileName);
+
+    if (Def.Recent > 0) and (Feed.Count > Def.Recent) then
+    begin
+      List := TList.Create;
+      try
+        for i := 0 to Feed.Count - 1 do
+          List.Add(Feed[i]);
+        List.Sort(DoSortFeed);
+        for i := Def.Recent to List.Count - 1 do
+          TCnFeedItem(List[i]).Free;
+      finally
+        List.Free;
+      end;   
+    end;  
+  end;
+end;
+
+function TCnFeedWizard.UpdateFeeds(ForceUpdate: Boolean): Boolean;
+var
+  i, j: Integer;
+  Offcial: TCnFeedDefine;
+  hMutex: THandle;
+begin
+  Result := False;
+
+  hMutex := CreateMutex(nil, False, SCnUpdateFeedMutex);
+  if GetLastError = ERROR_ALREADY_EXISTS then
+    Exit;
+    
+  try
+    if ForceUpdate then
+    begin
+      FFeeds.Clear;
+    end;  
+
+    Offcial.IDStr := SCnOfficalFeedIDStr;
+    Offcial.Url := SCnOfficalFeedUrl;
+    Offcial.CheckPeriod := SCnOfficalFeedPeriod;
+    Offcial.Recent := SCnOfficalFeedRecent;
+    if DoUpdateFeed(Offcial, ForceUpdate) then
+      Result := True;
+
+    for i := Low(FFeedDef) to High(FFeedDef) do
+      if DoUpdateFeed(FFeedDef[i], ForceUpdate) then
+        Result := True;
+
+    if Result then
+    begin
+      FCurFeed := nil;
+      FSortedFeeds.Clear;
+      for i := 0 to FeedCount - 1 do
+        for j := 0 to Feeds[i].Count - 1 do
+          FSortedFeeds.Add(Feeds[i][j]);
+      FSortedFeeds.Sort(DoSortFeed);
+    end;
+  finally
+    if hMutex <> 0 then
+    begin
+      ReleaseMutex(hMutex);
+      CloseHandle(hMutex);
+    end;
+  end;
 end;
 
 initialization
---
+++ Source/Utils/CnFeedParser.pas
@@ -0,0 +1,189 @@
+{******************************************************************************}
+{                       CnPack For Delphi/C++Builder                           }
+{                     中国人自己的开放源码第三方开发包                         }
+{                   (C)Copyright 2001-2010 CnPack 开发组                       }
+{                   ------------------------------------                       }
+{                                                                              }
+{            本开发包是开源的自由软件，您可以遵照 CnPack 的发布协议来修        }
+{        改和重新发布这一程序。                                                }
+{                                                                              }
+{            发布这一开发包的目的是希望它有用，但没有任何担保。甚至没有        }
+{        适合特定目的而隐含的担保。更详细的情况请参阅 CnPack 发布协议。        }
+{                                                                              }
+{            您应该已经和开发包一起收到一份 CnPack 发布协议的副本。如果        }
+{        还没有，可访问我们的网站：                                            }
+{                                                                              }
+{            网站地址：http://www.cnpack.org                                   }
+{            电子邮件：master@cnpack.org                                       }
+{                                                                              }
+{******************************************************************************}
+
+unit CnFeedParser;
+{ |<PRE>
+================================================================================
+* 软件名称：CnPack IDE 专家包
+* 单元名称：RSS Parser 单元
+* 单元作者：周劲羽 (zjy@cnpack.org)
+* 备    注：
+* 开发平台：PWinXP SP3 + Delphi 7.1
+* 兼容测试：
+* 本 地 化：该单元中的字符串支持本地化处理方式
+* 单元标识：$Id: $
+* 修改记录：2010.04.08
+*               创建单元
+================================================================================
+|</PRE>}
+
+interface
+
+{$I CnWizards.inc}
+
+uses
+  Windows, SysUtils, Classes;
+
+type
+
+  TCnFeedItem = class(TCollectionItem)
+  private
+    FPubDate: TDateTime;
+    FDescription: WideString;
+    FCategory: WideString;
+    FTitle: WideString;
+    FAuthor: WideString;
+    FLink: WideString;
+  published
+    property Title: WideString read FTitle write FTitle;
+    property Link: WideString read FLink write FLink;
+    property Description: WideString read FDescription write FDescription;
+    property Category: WideString read FCategory write FCategory;
+    property PubDate: TDateTime read FPubDate write FPubDate;
+    property Author: WideString read FAuthor write FAuthor;
+  end;
+
+  TCnFeedChannel = class(TCollection)
+  private
+    FLastBuildDate: TDateTime;
+    FPubDate: TDateTime;
+    FDescription: WideString;
+    FTitle: WideString;
+    FLanguage: WideString;
+    FLink: WideString;
+    FIDStr: WideString;
+    function GetItems(Index: Integer): TCnFeedItem;
+    procedure SetItems(Index: Integer; const Value: TCnFeedItem);
+  public
+    constructor Create;
+    procedure LoadFromStream(Stream: TStream);
+    procedure LoadFromFile(const FileName: string);
+
+    property Items[Index: Integer]: TCnFeedItem read GetItems write SetItems; default;
+  published
+    property IDStr: WideString read FIDStr write FIDStr;
+    property Title: WideString read FTitle write FTitle;
+    property Link: WideString read FLink write FLink;
+    property Description: WideString read FDescription write FDescription;
+    property Language: WideString read FLanguage write FLanguage;
+    property PubDate: TDateTime read FPubDate write FPubDate;
+    property LastBuildDate: TDateTime read FLastBuildDate write FLastBuildDate;
+  end;
+
+implementation
+
+uses
+  OmniXML, OmniXMLUtils;
+
+{ TCnFeedChannel }                                 
+
+constructor TCnFeedChannel.Create;
+begin
+  inherited Create(TCnFeedItem);
+end;
+
+function TCnFeedChannel.GetItems(Index: Integer): TCnFeedItem;
+begin
+  Result := TCnFeedItem(inherited Items[Index]);
+end;
+
+procedure TCnFeedChannel.LoadFromFile(const FileName: string);
+var
+  Stream: TMemoryStream;
+begin
+  Stream := TMemoryStream.Create;
+  try
+    Stream.LoadFromFile(FileName);
+    LoadFromStream(Stream);
+  finally
+    Stream.Free;
+  end;
+end;
+
+procedure TCnFeedChannel.LoadFromStream(Stream: TStream);
+var
+  XML: IXMLDocument;
+  Node, Item: IXMLNode;
+  i: Integer;
+  
+  function FeedStrToDateTime(S: WideString): TDateTime;
+  begin
+    if not XMLStrToDateTime(S, Result) then
+      Result := Now; // todo: FeedStrToDateTime
+  end;  
+begin
+  try
+    Clear;
+    XML := CreateXMLDoc;
+    if XML.LoadFromStream(Stream) then
+    begin
+      // RSS 2.0
+      Node := FindNode(XML, 'rss');
+      if Node <> nil then
+      begin
+        Node := FindNode(Node, 'channel');
+        if Node <> nil then
+        begin
+          Title := GetNodeTextStr(Node, 'title', '');
+          Link := GetNodeTextStr(Node, 'link', '');
+          Description := GetNodeTextStr(Node, 'description', '');
+          Language := GetNodeTextStr(Node, 'language', '');
+          PubDate := FeedStrToDateTime(GetNodeTextStr(Node, 'pubDate', ''));
+          LastBuildDate := FeedStrToDateTime(GetNodeTextStr(Node, 'lastBuildDate', ''));
+
+          for i := 0 to Node.ChildNodes.Length - 1 do
+          begin
+            if SameText(Node.ChildNodes.Item[i].NodeName, 'item') then
+            begin
+              Item := Node.ChildNodes.Item[i];
+              with TCnFeedItem(Add) do
+              begin
+                Title := GetNodeTextStr(Item, 'title', '');
+                Link := GetNodeTextStr(Item, 'link', '');
+                Description := GetNodeTextStr(Item, 'description', '');
+                Category := GetNodeTextStr(Item, 'category', '');
+                Author := GetNodeTextStr(Item, 'author', '');
+                PubDate := FeedStrToDateTime(GetNodeTextStr(Item, 'pubDate', ''));
+              end;
+            end;
+          end;
+          Exit;
+        end;
+      end;
+
+      // ATOM
+      Node := FindNode(XML, 'feed');
+      if Node <> nil then
+      begin
+        // todo: Support ATOM format
+      end;  
+    end;
+  except
+    ;
+  end;
+end;
+
+procedure TCnFeedChannel.SetItems(Index: Integer;
+  const Value: TCnFeedItem);
+begin
+  inherited Items[Index] := Value;
+end;
+
+end.
