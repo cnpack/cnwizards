e22b7283f0e72b93bce810f07c2389f6ad42f8ab
shanzhashu <shanzhashu@163.com>
2011-08-28 11:18:45 +0000

    * [版本检查] 更新至XE2 Beta 12.

--- Source/Misc/CnIDEVersion.pas
+++ Source/Misc/CnIDEVersion.pas
@@ -226,7 +226,7 @@ end;
 function IsDelphi16IdeVersionLatest: Boolean;
 const
   CoreIdeLatest: TVersionNumber =
-    (Major: 16; Minor: 0; Release: 4252; Build: 43494);
+    (Major: 16; Minor: 0; Release: 4256; Build: 43595);
 var
   ReadFileVersion: TVersionNumber;
 begin
