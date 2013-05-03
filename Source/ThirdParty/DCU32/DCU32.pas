f0ee37a954eb4545d194be6f78987b3b0fdc17d0
shanzhashu <shanzhashu@163.com>
2013-05-03 02:52:20 +0000

    * [Thrid Party] Add XE4 dcu parsing Support to DCU32.

--- Source/ThirdParty/DCU32/DCU32.pas
+++ Source/ThirdParty/DCU32/DCU32.pas
@@ -52,6 +52,7 @@ const {My own (AX) codes for Delphi/Kylix versions}
   verD15=15; // Added by Liu Xiao. Delphi 2011(XE).
   verD16=16; // Added by Liu Xiao. Delphi 2012(XE2).
   verD17=17; // Added by shenloqi. Delphi 2013(XE3).
+  verDXE4=18; // Added by Liu Xiao. Delphi XE4.
   verK1=100; //Kylix 1.0
   verK2=101; //Kylix 2.0
   verK3=103; //Kylix 2.0
@@ -2884,6 +2885,7 @@ begin
       $1600034B: FVer := verD15; // Added by Liu Xiao for Delphi 2011(XE).
       $1700034B, $1780034B: FVer := verD16; // Added by Liu Xiao for Delphi 2012(XE2).
       $1800034B, $1800234B, $1800044B: FVer := verD17; // Added by shenloqi for Delphi 2013(XE3).
+      $1900034B: FVer := verDXE4; // Added by LiuXiao
       ulong($F21F148C): FVer := verK1; //Kylix 1.0
       $0E1011DD,$0E0001DD: FVer := verK2; //Kylix 2.0
       $0F1001DD,$0F0001DD: FVer := verK3; //Kylix 3.0
