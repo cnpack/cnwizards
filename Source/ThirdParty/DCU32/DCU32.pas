e186d87ea3ffc65c1d0def7a130471ab7d66db0e
shanzhashu <shanzhashu@163.com>
2013-09-18 23:50:30 +0000

    * [DCU32] Add XE5 Dcu Format Support.

--- Source/ThirdParty/DCU32/DCU32.pas
+++ Source/ThirdParty/DCU32/DCU32.pas
@@ -53,6 +53,7 @@ const {My own (AX) codes for Delphi/Kylix versions}
   verD16=16; // Added by Liu Xiao. Delphi 2012(XE2).
   verD17=17; // Added by shenloqi. Delphi 2013(XE3).
   verDXE4=18; // Added by Liu Xiao. Delphi XE4.
+  verDXE5=19; // Added by Liu Xiao. Delphi XE5.
   verK1=100; //Kylix 1.0
   verK2=101; //Kylix 2.0
   verK3=103; //Kylix 2.0
@@ -2886,6 +2887,7 @@ begin
       $1700034B, $1780034B: FVer := verD16; // Added by Liu Xiao for Delphi 2012(XE2).
       $1800034B, $1800234B, $1800044B: FVer := verD17; // Added by shenloqi for Delphi 2013(XE3).
       $1900034B: FVer := verDXE4; // Added by LiuXiao
+      $1A00034B: FVer := verDXE5; // Added by LiuXiao
       ulong($F21F148C): FVer := verK1; //Kylix 1.0
       $0E1011DD,$0E0001DD: FVer := verK2; //Kylix 2.0
       $0F1001DD,$0F0001DD: FVer := verK3; //Kylix 3.0
