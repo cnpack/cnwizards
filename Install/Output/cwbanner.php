ab7801ce3765d6181665421113eed44370803991
liuxiao <liuxiao@cnpack.org>
2015-03-02 22:37:31 +0800

    * [Banner] Update for 1.0.5.

--- Install/Output/cwbanner.php
+++ Install/Output/cwbanner.php
@@ -16,7 +16,7 @@
         <a href="http://www.cnpack.org/index.php?lang=zh-cn" target="_blank"><img src="images/logo.gif" width="220" height="70" border=0 align="center"></a>
       </td>
       <td valign="middle" height="100%" class="pnlbody"><p>欢迎下载使用 CnPack 开发组的作品 CnWizards，这是由国人开发的开源免费的 IDE 增强工具，支持 Delphi / C++Builder / BDS / RAD Studio。<br>
-        <a href="http://www.cnpack.org/showdetail.php?id=753&lang=zh-cn" target="_blank">2014 年 10 月 01 日，CnWizards 发布新版 1.0.4.680 !</a><img border="0" src="images/new.gif"></p>
+        <a href="http://www.cnpack.org/showdetail.php?id=763&lang=zh-cn" target="_blank">2015 年 1 月 31 日，CnWizards 发布新版 1.0.5.693 !</a><img border="0" src="images/new.gif"></p>
       </td>
     </tr>
   </table>
