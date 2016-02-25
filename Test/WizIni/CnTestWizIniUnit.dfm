abbe09208305c8c48787333f388d2082a4a27443
liuxiao <liuxiao@cnpack.org>
2016-02-25 19:51:27 +0800

    * [Test Case] Add a Test Case for Wiz Ini.

---
+++ Test/WizIni/CnTestWizIniUnit.dfm
@@ -0,0 +1,138 @@
+object TestWizIniForm: TTestWizIniForm
+  Left = 192
+  Top = 107
+  BorderStyle = bsDialog
+  Caption = 'Test CnWizIni'
+  ClientHeight = 414
+  ClientWidth = 614
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 13
+  object btnLoadSetting: TButton
+    Left = 32
+    Top = 32
+    Width = 75
+    Height = 25
+    Caption = 'Load Setting'
+    TabOrder = 0
+    OnClick = btnLoadSettingClick
+  end
+  object grpSettings: TGroupBox
+    Left = 32
+    Top = 88
+    Width = 545
+    Height = 289
+    Caption = 'Settings'
+    TabOrder = 1
+    object lblSetting2: TLabel
+      Left = 184
+      Top = 32
+      Width = 78
+      Height = 13
+      Caption = 'Setting 2 Integer'
+    end
+    object lblSetting3: TLabel
+      Left = 376
+      Top = 32
+      Width = 72
+      Height = 13
+      Caption = 'Setting 3 String'
+    end
+    object lblSetting4: TLabel
+      Left = 24
+      Top = 112
+      Width = 87
+      Height = 13
+      Caption = 'Setting 4 Datetime'
+    end
+    object edtSetting2: TEdit
+      Left = 184
+      Top = 56
+      Width = 129
+      Height = 21
+      TabOrder = 0
+      Text = '0'
+    end
+    object udSetting2: TUpDown
+      Left = 313
+      Top = 56
+      Width = 15
+      Height = 21
+      Associate = edtSetting2
+      Min = -100
+      Position = 0
+      TabOrder = 1
+      Wrap = False
+    end
+    object edtSetting3: TEdit
+      Left = 376
+      Top = 56
+      Width = 145
+      Height = 21
+      TabOrder = 2
+    end
+    object dtpSetting4Date: TDateTimePicker
+      Left = 144
+      Top = 112
+      Width = 153
+      Height = 21
+      CalAlignment = dtaLeft
+      Date = 42425.4240267014
+      Time = 42425.4240267014
+      DateFormat = dfLong
+      DateMode = dmComboBox
+      Kind = dtkDate
+      ParseInput = False
+      TabOrder = 3
+    end
+    object dtpSetting4Time: TDateTimePicker
+      Left = 304
+      Top = 112
+      Width = 153
+      Height = 21
+      CalAlignment = dtaLeft
+      Date = 42425.4240267014
+      Time = 42425.4240267014
+      DateFormat = dfShort
+      DateMode = dmComboBox
+      Kind = dtkTime
+      ParseInput = False
+      TabOrder = 4
+    end
+    object chkSetting1: TCheckBox
+      Left = 24
+      Top = 48
+      Width = 113
+      Height = 17
+      Caption = 'Setting 1 Boolean'
+      TabOrder = 5
+    end
+  end
+  object btnSaveSetting: TButton
+    Left = 504
+    Top = 32
+    Width = 75
+    Height = 25
+    Caption = 'Save Setting'
+    TabOrder = 2
+    OnClick = btnSaveSettingClick
+  end
+  object btnDump: TButton
+    Left = 256
+    Top = 32
+    Width = 75
+    Height = 25
+    Caption = 'Dump Defaults'
+    TabOrder = 3
+    OnClick = btnDumpClick
+  end
+end
---
+++ Test/WizIni/CnTestWizIniUnit.pas
@@ -0,0 +1,126 @@
+unit CnTestWizIniUnit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, CnWizIni, CnHashMap, CnWizConsts, CnConsts, ComCtrls;
+
+type
+  TTestWizIniForm = class(TForm)
+    btnLoadSetting: TButton;
+    grpSettings: TGroupBox;
+    btnSaveSetting: TButton;
+    btnDump: TButton;
+    edtSetting2: TEdit;
+    udSetting2: TUpDown;
+    lblSetting2: TLabel;
+    lblSetting3: TLabel;
+    edtSetting3: TEdit;
+    dtpSetting4Date: TDateTimePicker;
+    lblSetting4: TLabel;
+    dtpSetting4Time: TDateTimePicker;
+    chkSetting1: TCheckBox;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure btnDumpClick(Sender: TObject);
+    procedure btnLoadSettingClick(Sender: TObject);
+    procedure btnSaveSettingClick(Sender: TObject);
+  private
+    FIni: TCnWizIniFile;
+    FMap: TCnStrToVariantHashMap;
+    procedure ShowValues;
+  public
+    { Public declarations }
+  end;
+
+var
+  TestWizIniForm: TTestWizIniForm;
+
+implementation
+
+{$R *.DFM}
+
+const
+  csTestSettingKey1 = 'TestSettingKey1';  // Boolean
+  csTestSettingKey2 = 'TestSettingKey2';  // Integer
+  csTestSettingKey3 = 'TestSettingKey3';  // String
+  csTestSettingKey4 = 'TestSettingKey4';  // TDatetime
+
+  DefaultSetting1Boolean = True;
+  DefaultSetting2Integer = 32;
+  DefaultSetting3String = 'Test Me';
+  DefaultSetting4Datetime = '2016-2-24 10:32:34.344';
+
+var
+  Setting1: Boolean = False;
+  Setting2: Integer = -1;
+  Setting3: string = 'Init';
+  Setting4: TDateTime = 0;
+
+procedure TTestWizIniForm.FormCreate(Sender: TObject);
+var
+  Path: string;
+begin
+  FMap := TCnStrToVariantHashMap.Create();
+
+  Path := IncludeTrailingBackslash(IncludeTrailingBackslash((SCnPackRegPath)
+    + SCnWizardRegPath)) + 'TestWizIni';
+  FIni := TCnWizIniFile.Create(Path, KEY_ALL_ACCESS, FMap);
+
+  ShowValues;
+end;
+
+procedure TTestWizIniForm.FormDestroy(Sender: TObject);
+begin
+  FIni.Free;
+  FMap.Free;
+end;
+
+procedure TTestWizIniForm.btnDumpClick(Sender: TObject);
+var
+  List: TStrings;
+  Key, Value: Variant;
+begin
+  List := TStringList.Create;
+  try
+    FMap.StartEnum;
+    while FMap.GetNext(Key, Value) do
+      List.Add(VarToStr(Key) + ':' + VarToStr(Value));
+
+    ShowMessage(List.Text);
+  finally
+    List.Free;
+  end;
+end;
+
+procedure TTestWizIniForm.ShowValues;
+begin
+  chkSetting1.Checked := Setting1;
+  udSetting2.Position := Setting2;
+  edtSetting3.Text := Setting3;
+  dtpSetting4Date.DateTime := Setting4;
+  dtpSetting4Time.DateTime := Setting4;
+end;
+
+procedure TTestWizIniForm.btnLoadSettingClick(Sender: TObject);
+begin
+  Setting1 := FIni.ReadBool('', csTestSettingKey1, DefaultSetting1Boolean);
+  Setting2 := FIni.ReadInteger('', csTestSettingKey2, DefaultSetting2Integer);
+  Setting3 := FIni.ReadString('', csTestSettingKey3, DefaultSetting3String);
+  Setting4 := FIni.ReadDateTime('', csTestSettingKey4, StrToDateTime(DefaultSetting4Datetime));
+
+  ShowValues;
+end;
+
+procedure TTestWizIniForm.btnSaveSettingClick(Sender: TObject);
+begin
+  FIni.WriteBool('', csTestSettingKey1, Setting1);
+  FIni.WriteInteger('', csTestSettingKey2, Setting2);
+  FIni.WriteString('', csTestSettingKey3, Setting3);
+  FIni.WriteDateTime('', csTestSettingKey4, Setting4);
+
+  ShowMessage('Settings Saved. Please Check Registry.');
+end;
+
+end.
