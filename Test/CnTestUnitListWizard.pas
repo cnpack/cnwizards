b31c256bc64b0074b7851dfd85c266b547b8c426
liuxiao <liuxiao@cnpack.org>
2016-03-04 17:16:04 +0800

    * [Test Case] Implement Uses Add for Testing.

--- Test/CnTestUnitListWizard.pas
+++ Test/CnTestUnitListWizard.pas
@@ -53,7 +53,8 @@ type
 
   TCnTestUnitListWizard = class(TCnMenuWizard)
   private
-
+    function SearchInsertPos(IsIntf: Boolean; out HasUses: Boolean; out CharPos: TOTACharPos): Boolean;
+    procedure SearchInsertPosW(IsIntf: Boolean; out HasUses: Boolean);
   protected
     function GetHasConfig: Boolean; override;
   public
@@ -71,7 +72,7 @@ type
 implementation
 
 uses
-  CnDebug;
+  CnDebug, mPasLex, CnCommon;
 
 //==============================================================================
 // 测试 TUnitNameList 相关功能的菜单专家
@@ -92,6 +93,10 @@ var
   List: TUnitNameList;
   Names: TStringList;
   Paths: TStringList;
+  HasUses: Boolean;
+  CharPos: TOTACharPos;
+  LinearPos: LongInt;
+  EditView: IOTAEditView;
 begin
   List := TUnitNameList.Create;
   Names := TStringList.Create;
@@ -105,6 +110,8 @@ begin
     List.ExportToStringList(Names, Paths);
 
     ShowMessage('Found Units Count: ' + IntToStr(Names.Count));
+
+    // 此时得到了所有可引用的单元列表
 
     CnOtaSaveCurrentEditorToStream(Stream, False);
     ParseUnitUses(PAnsiChar(Stream.Memory), UsesList);
@@ -123,6 +130,53 @@ begin
     ShowMessage('Found Units not used: ' + IntToStr(Names.Count));
     for I := 0 to Names.Count - 1 do
       CnDebugger.LogFmt('%d. %s in %s', [Integer(Names.Objects[I]), Names[I], Paths[I]]);
+
+    // 此时剔除已引用的，得到了可引用的单元列表
+    if Names.Count = 0 then
+      Exit;
+    EditView := CnOtaGetTopMostEditView;
+
+    // 插入至 interface 处的 uses，还得处理无 uses 的情况
+    if not SearchInsertPos(False, HasUses, CharPos) then
+    begin
+      ErrorDlg('Can NOT Find an Insert Position for implementation uses.');
+      Exit;
+    end;
+
+    // 已经得到行 1 列 0 开始的 CharPos，用 EditView.CharPosToPos(CharPos) 转换为线性;
+    LinearPos := EditView.CharPosToPos(CharPos);
+
+    if HasUses then
+    begin
+      ShowMessage('Will insert ' + Names[0] + ' to Position ' + IntToStr(CharPos.Line) + ':' + IntToStr(CharPos.CharIndex));
+      CnOtaInsertTextIntoEditorAtPos(', ' + Names[0], LinearPos);
+    end
+    else
+    begin
+      ShowMessage('Will insert uses ' + Names[0] + ' after interface. Line ' + IntToStr(CharPos.Line));
+      CnOtaInsertTextIntoEditorAtPos(#13#10#13#10 + 'uses' + #13#10 + '  ' + Names[0] + ';', LinearPos);
+    end;
+
+    if Names.Count = 1 then
+      Exit;
+
+    if not SearchInsertPos(True, HasUses, CharPos) then
+    begin
+      ErrorDlg('Can NOT Find an Insert Position for interface uses.');
+      Exit;
+    end;
+
+    LinearPos := EditView.CharPosToPos(CharPos);
+    if HasUses then
+    begin
+      ShowMessage('Will insert ' + Names[1] + ' to Position ' + IntToStr(CharPos.Line) + ':' + IntToStr(CharPos.CharIndex));
+      CnOtaInsertTextIntoEditorAtPos(', ' + Names[1], LinearPos);
+    end
+    else
+    begin
+      ShowMessage('Will insert uses ' + Names[1] + ' after implementation. Line ' + IntToStr(CharPos.Line));
+      CnOtaInsertTextIntoEditorAtPos(#13#10#13#10 + 'uses' + #13#10 + '  ' + Names[1] + ';', LinearPos);
+    end;
   finally
     UsesList.Free;
     Stream.Free;
@@ -171,6 +225,95 @@ begin
 end;
 
 procedure TCnTestUnitListWizard.SaveSettings(Ini: TCustomIniFile);
+begin
+
+end;
+
+function TCnTestUnitListWizard.SearchInsertPos(IsIntf: Boolean; out HasUses: Boolean;
+  out CharPos: TOTACharPos): Boolean;
+var
+  Stream: TMemoryStream;
+  Lex: TmwPasLex;
+  FoundUses: Boolean;
+  IntfMeet: Boolean;
+  ImplMeet: Boolean;
+  IntfLine, ImplLine: Integer;
+begin
+  Result := False;
+  Stream := TMemoryStream.Create;
+  CnOtaSaveCurrentEditorToStream(Stream, False);
+
+  Lex := TmwPasLex.Create;
+  IntfMeet := False;
+  ImplMeet := False;
+  HasUses := False;
+  IntfLine := 0;
+  ImplLine := 0;
+  
+  CharPos.Line := 0;
+  CharPos.CharIndex := -1;
+
+  try
+    Lex.Origin := PAnsiChar(Stream.Memory);
+    while Lex.TokenID <> tkNull do
+    begin
+      case Lex.TokenID of
+      tkUses:
+        begin
+          if (IsIntf and IntfMeet) or (not IsIntf and ImplMeet) then
+          begin
+            HasUses := True; // 到达了自己需要的 uses 处
+            while not (Lex.TokenID in [tkNull, tkSemiColon]) do
+              Lex.Next;
+
+            if Lex.TokenID = tkSemiColon then
+            begin
+              // 插入位置就在分号前
+              Result := True;
+              CharPos.Line := Lex.LineNumber + 1;
+              CharPos.CharIndex := Lex.TokenPos - Lex.LinePos;
+              Exit;
+            end
+            else // uses 后找不着分号，出错
+            begin
+              Result := False;
+              Exit;
+            end;
+          end;
+        end;
+      tkInterface:
+        begin
+          IntfMeet := True;
+          IntfLine := Lex.LineNumber + 1;
+        end;
+      tkImplementation:
+        begin
+          ImplMeet := True;
+          ImplLine := Lex.LineNumber + 1;
+        end;
+      end;
+      Lex.Next;
+    end;
+
+    if IsIntf and IntfMeet then
+    begin
+      Result := True;
+      CharPos.Line := IntfLine;
+      CharPos.CharIndex := Length('interface');
+    end
+    else if not IsIntf and ImplMeet then
+    begin
+      Result := True;
+      CharPos.Line := ImplLine;
+      CharPos.CharIndex := Length('implementation');
+    end;
+  finally
+    Lex.Free;
+    Stream.Free;
+  end;
+end;
+
+procedure TCnTestUnitListWizard.SearchInsertPosW;
 begin
 
 end;
