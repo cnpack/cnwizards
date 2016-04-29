8615d8b7215099e96390f13675a4db9c48116852
liuxiao <liuxiao@cnpack.org>
2016-04-29 16:18:10 +0800

    * [IDEBRTool] Add Support Partly to 10.1 Berlin.

--- Tools/CnIdeBRTool/CleanClass.pas
+++ Tools/CnIdeBRTool/CleanClass.pas
@@ -97,7 +97,7 @@ const
      'RAD Studio 2007', 'RAD Studio 2009', 'RAD Studio 2010', 'RAD Studio XE',
      'RAD Studio XE2', 'RAD Studio XE3', 'RAD Studio XE4', 'RAD Studio XE5',
      'RAD Studio XE6', 'RAD Studio XE7', 'RAD Studio XE8', 'RAD Studio 10 Seattle',
-     'C++Builder 5', 'C++Builder 6');
+     'RAD Studio 10.1 Berlin', 'C++Builder 5', 'C++Builder 6');
 
 procedure CreateIDEHistories;
 var
