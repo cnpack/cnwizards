4b53f18f3adb1cec5e26e1a6793e26aa326d094b
LiuXiao <liuxiao@cnpack.org>
2022-06-24 17:29:53 +0800

    * [Test Case] Add a Debugger Visualizer Test Case.

---
+++ Test/CnTestDebuggerVisualizerWizard.pas
@@ -0,0 +1,213 @@
+{******************************************************************************}
+{                       CnPack For Delphi/C++Builder                           }
+{                     中国人自己的开放源码第三方开发包                         }
+{                   (C)Copyright 2001-2022 CnPack 开发组                       }
+{                   ------------------------------------                       }
+{                                                                              }
+{            本开发包是开源的自由软件，您可以遵照 CnPack 的发布协议来修        }
+{        改和重新发布这一程序。                                                }
+{                                                                              }
+{            发布这一开发包的目的是希望它有用，但没有任何担保。甚至没有        }
+{        适合特定目的而隐含的担保。更详细的情况请参阅 CnPack 发布协议。        }
+{                                                                              }
+{            您应该已经和开发包一起收到一份 CnPack 发布协议的副本。如果        }
+{        还没有，可访问我们的网站：                                            }
+{                                                                              }
+{            网站地址：http://www.cnpack.org                                   }
+{            电子邮件：master@cnpack.org                                       }
+{                                                                              }
+{******************************************************************************}
+
+unit CnTestDebuggerVisualizerWizard;
+{ |<PRE>
+================================================================================
+* 软件名称：CnPack IDE 专家包测试用例
+* 单元名称：测试 DebuggerVisualizer 的测试用例单元
+* 单元作者：CnPack 开发组
+* 备    注：测试 DebuggerVisualizer 以更改调试内容显示机制
+            所在处的位置类型与文字，需要在 D5/2007/2009 等测试通过。
+* 开发平台：Win7 + Delphi XE2
+* 兼容测试：PWin9X/2000/XP + Delphi All
+* 本 地 化：该窗体中的字符串暂不支持本地化处理方式
+* 修改记录：2022.06.24 V1.0
+*               创建单元
+================================================================================
+|</PRE>}
+
+interface
+
+{$I CnWizards.inc}
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  ToolsAPI, IniFiles, CnCommon, CnWizClasses, CnWizUtils, CnWizConsts;
+
+type
+
+//==============================================================================
+// 测试 DebuggerVisualizer 的菜单专家
+//==============================================================================
+
+{ TCnTestDebuggerVisualizerWizard }
+
+  TCnTestDebuggerVisualizerWizard = class(TCnMenuWizard)
+  private
+    FRegistered: Boolean;
+    FVisualizer: IOTADebuggerVisualizerValueReplacer;
+  protected
+    function GetHasConfig: Boolean; override;
+  public
+    function GetState: TWizardState; override;
+    procedure Config; override;
+    procedure LoadSettings(Ini: TCustomIniFile); override;
+    procedure SaveSettings(Ini: TCustomIniFile); override;
+    class procedure GetWizardInfo(var Name, Author, Email, Comment: string); override;
+    function GetCaption: string; override;
+    function GetHint: string; override;
+    function GetDefShortCut: TShortCut; override;
+    procedure Execute; override;
+  end;
+
+  TCnTestDebuggerVisualizerValueReplacer = class(TInterfacedObject, IOTADebuggerVisualizerValueReplacer)
+  public
+    function GetSupportedTypeCount: Integer;
+    procedure GetSupportedType(Index: Integer; var TypeName: string;
+      var AllDescendants: Boolean);
+    function GetVisualizerIdentifier: string;
+    function GetVisualizerName: string;
+    function GetVisualizerDescription: string;
+
+    function GetReplacementValue(const Expression, TypeName, EvalResult: string): string;
+  end;
+
+implementation
+
+uses
+  CnDebug;
+
+//==============================================================================
+// 测试 DebuggerVisualizer 的菜单专家
+//==============================================================================
+
+{ TCnTestDebuggerVisualizerWizard }
+
+procedure TCnTestDebuggerVisualizerWizard.Config;
+begin
+  ShowMessage('No option for this test case.');
+end;
+
+procedure TCnTestDebuggerVisualizerWizard.Execute;
+var
+  ID: IOTADebuggerServices;
+begin
+  if not Supports(BorlandIDEServices, IOTADebuggerServices, ID) then
+    Exit;
+
+  if FVisualizer = nil then
+    FVisualizer := TCnTestDebuggerVisualizerValueReplacer.Create;
+
+  if not FRegistered then
+  begin
+    ID.RegisterDebugVisualizer(FVisualizer);
+    FRegistered := True;
+    ShowMessage('Debugger Visualizer Registered');
+  end
+  else
+  begin
+    ID.UnregisterDebugVisualizer(FVisualizer);
+    FRegistered := False;
+    ShowMessage('Debugger Visualizer UnRegistered');
+  end;
+end;
+
+function TCnTestDebuggerVisualizerWizard.GetCaption: string;
+begin
+  Result := 'Test DebuggerVisualizer';
+end;
+
+function TCnTestDebuggerVisualizerWizard.GetDefShortCut: TShortCut;
+begin
+  Result := 0;
+end;
+
+function TCnTestDebuggerVisualizerWizard.GetHasConfig: Boolean;
+begin
+  Result := True;
+end;
+
+function TCnTestDebuggerVisualizerWizard.GetHint: string;
+begin
+  Result := 'Test hint';
+end;
+
+function TCnTestDebuggerVisualizerWizard.GetState: TWizardState;
+begin
+  Result := [wsEnabled];
+end;
+
+class procedure TCnTestDebuggerVisualizerWizard.GetWizardInfo(var Name, Author, Email, Comment: string);
+begin
+  Name := 'Test DebuggerVisualizer Menu Wizard';
+  Author := 'Liu Xiao';
+  Email := 'master@cnpack.org';
+  Comment := 'Test for DebuggerVisualizer under Delphi XE or above';
+end;
+
+procedure TCnTestDebuggerVisualizerWizard.LoadSettings(Ini: TCustomIniFile);
+begin
+
+end;
+
+procedure TCnTestDebuggerVisualizerWizard.SaveSettings(Ini: TCustomIniFile);
+begin
+
+end;
+
+{ TCnTestDebuggerVisualizerValueReplacer }
+
+function TCnTestDebuggerVisualizerValueReplacer.GetReplacementValue(
+  const Expression, TypeName, EvalResult: string): string;
+begin
+  CnDebugger.LogFmt('DebuggerVisualizerValueReplacer get %s: %s, Display %s',
+    [Expression, TypeName, EvalResult]);
+  Result := EvalResult + ' - From CnPack';
+end;
+
+procedure TCnTestDebuggerVisualizerValueReplacer.GetSupportedType(
+  Index: Integer; var TypeName: string; var AllDescendants: Boolean);
+begin
+  AllDescendants := False;
+  case Index of
+    0: TypeName := 'TCnBigNumber';
+    1: TypeName := 'TCnBigNumberPolynomial';
+    2: TypeName := 'TCnEccPoint';
+    3: TypeName := 'TCnEcc3Point';
+  end;
+end;
+
+function TCnTestDebuggerVisualizerValueReplacer.GetSupportedTypeCount: Integer;
+begin
+  Result := 4;
+end;
+
+function TCnTestDebuggerVisualizerValueReplacer.GetVisualizerDescription: string;
+begin
+  Result := 'CnPack CnVcl Debugger Visualizer for some Classes.'
+end;
+
+function TCnTestDebuggerVisualizerValueReplacer.GetVisualizerIdentifier: string;
+begin
+  Result := 'CnVclVisualizer';
+end;
+
+function TCnTestDebuggerVisualizerValueReplacer.GetVisualizerName: string;
+begin
+  Result := 'CnPack CnVcl Visualizer'
+end;
+
+initialization
+{$IFDEF IDE_HAS_DEBUGGERVISUALIZER}
+  RegisterCnWizard(TCnTestDebuggerVisualizerWizard); // 注册此测试专家
+{$ENDIF}
+
+end.
