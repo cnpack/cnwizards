5f54c27032bc6fbca46c039455e54d5f180783b8
LiuXiao <liuxiao@cnpack.org>
2023-03-06 23:09:10 +0800

    * [Test Case] Update a Pas Demo for Dcu32.

--- Test/Dcu32/TestDcus/CnUnitTest.pas
+++ Test/Dcu32/TestDcus/CnUnitTest.pas
@@ -6,10 +6,40 @@ uses
   SysUtils, Classes;
 
 type
-  TTestObject = class
+  TTestEnum = (teTest1, teTest2, teTest3);
+
+  TTestRecord = packed record
 
   end;
 
+  TTestObject = class
+  private
+    FField1: Boolean;
+  public
+    procedure TestObject;
+
+    property Field1: Boolean read FField1 write FField1;
+  end;
+
+function TestFunction(var A: Integer; const B: string): Boolean;
+
+procedure TestProcedure(C: TObject);
+
 implementation
+
+function TestFunction(var A: Integer; const B: string): Boolean;
+begin
+  Result := False;
+end;
+
+procedure TestProcedure(C: TObject);
+begin
+
+end;
+
+procedure TTestObject.TestObject;
+begin
+
+end;
 
 end.
