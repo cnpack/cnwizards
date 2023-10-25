af3e722bcad04aa926b01ec0321b283eca5f6da4
Liu Xiao <liuxiao@cnpack.org>
2023-10-25 13:27:02 +0800

    * [MultiLine Editor] Fix an Icon Size Problem in HDPI.

--- Source/DesignEditors/CnMultiLineEditorFrm.pas
+++ Source/DesignEditors/CnMultiLineEditorFrm.pas
@@ -30,23 +30,23 @@ unit CnMultiLineEditorFrm;
 * 本 地 化：该单元和窗体中的字符串已经本地化处理方式
 * 修改记录：
 *           2005.08.01
-*               修正SetFont时每次都创建一个新的TFontDialog的BUG
+*               修正 SetFont 时每次都创建一个新的 TFontDialog 的 BUG
 *               调整 自动换行 为 隐藏/显示水平滚动条
 *           2003.10.31
-*               增加了Reload功能
+*               增加了 Reload 功能
 *               修正块左移包含空行出错的BUG
 *               自定义格式化前后缀只要有一个不为空就可以生效
 *           2003.10.29
-*               增加了去除引用工具，采用GetSelText以便更好的适应不同环境
+*               增加了去除引用工具，采用 GetSelText 以便更好的适应不同环境
 *           2003.10.18
 *               增加了一些辅助工具
 *           2003.09.17 V1.4
-*               修改了全部替换死循环的BUG
+*               修改了全部替换死循环的 BUG
 *           2003.06.28 V1.3
-*               在memEdit的EM_SETSEL中更新光标的位置
+*               在 memEdit 的 EM_SETSEL 中更新光标的位置
 *           2003.03.14 V1.2
-*               注释了Font对话框的文字设定
-*               增加了Height,Weight的save,load
+*               注释了 Font 对话框的文字设定
+*               增加了 Height, Weight 的 save, load
 *               字符串符合了本地化处理方式
 *               增加了光标位置指示
 *               增加了使用Delphi的Editor进行编辑
@@ -464,6 +464,7 @@ begin
     Free;
   end;
 
+  WizOptions.ResetToolbarWithLargeIcons(tbrMain);
   FOldMemoWndProc := memEdit.WindowProc;
   memEdit.WindowProc := MemoWndProc;
 end;
