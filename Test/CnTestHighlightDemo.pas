82860478e01a0dea91aea10f48e9921c4027fe3b
Liu Xiao <liuxiao@cnpack.org>
2024-08-03 09:23:47 +0800

    + [Test Case] Add a Highlight Test Source File.

---
+++ Test/CnTestHighlightDemo.pas
@@ -0,0 +1,19 @@
+unit CnTestHighlightDemo;
+// 本单元用于不同 IDE 版本中的高亮显示测试，不参与编译
+interface
+
+implementation
+
+procedure Test1;
+begin
+if h = '▲' then
+if h = '△' then
+  Exit;
+
+  var 吃饭: string := 'Test';
+  吃饭 := '';
+
+end;
+
+
+end.
