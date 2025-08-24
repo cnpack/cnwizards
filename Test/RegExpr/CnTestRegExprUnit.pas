00c3aeae887b5fd5714f233302fe389bc65f2a7e
Liu Xiao <master@cnpack.org>
2025-08-24 19:45:44 +0800

    * [ThirdParty] Adjust a Related Test Unit.

--- Test/RegExpr/CnTestRegExprUnit.pas
+++ Test/RegExpr/CnTestRegExprUnit.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, RegExpr;
+  StdCtrls, AsRegExpr;
 
 type
   TTestRegExprForm = class(TForm)
