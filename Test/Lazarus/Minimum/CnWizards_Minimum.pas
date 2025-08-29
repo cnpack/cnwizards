b3964a652539acd1a3de7a02daed5edd5c8bfc78
Liu Xiao <master@cnpack.org>
2025-08-30 00:20:17 +0800

    # [Test Case] Add a Minimum lpk for Lazarus.

---
+++ Test/Lazarus/Minimum/CnWizards_Minimum.pas
@@ -0,0 +1,31 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit CnWizards_L4;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  CnLazPkgEntry, CnWizConsts, CnWizCompilerConst, CnWizOptions, CnWizShortCut, 
+  CnWizMenuAction, CnWizClasses, CnPasCodeParser, CnPasWideLex, 
+  CnWidePasParser, mPasLex, CnWizManager, CnWizIdeUtils, CnWizUtils, 
+  CnWizAbout, CnWizAboutFrm, CnMessageBoxWizard, CnWizConfigFrm, CnWizIdeDock, 
+  CnAICoderEngine, CnAICoderEngineImpl, CnAICoderNetClient, CnAICoderConfig, 
+  CnAICoderChatFrm, CnAICoderWizard, CnCodingToolsetWizard, 
+  CnProjectViewBaseFrm, CnEditorOpenFile, CnEditorOpenFileFrm, CnFloatWindow, 
+  CnSourceCropper, CnPascalAST, CnLineParser, CnWizEditFiler, 
+  CnProcListWizard, CnSrcEditorToolBar, CnFlatToolbarConfigFrm, 
+  CnTestFormDesignerWizard, CnComponentSelector, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('CnLazPkgEntry', @CnLazPkgEntry.Register);
+end;
+
+initialization
+  RegisterPackage('CnWizards_L4', @Register);
+end.
